e2fde87c4eb47f17a78ed48aa171dba4cb57f382
rajanbit <rajan22091998@gmail.com>
2026-01-08 10:43:53 +0530

    added ML models with hyperparameter for ML analysis

--- src/model.py
+++ src/model.py
@@ -4,8 +4,27 @@ import torch.nn as nn
 from torch.nn.functional import relu, elu
 from torch_geometric.nn import GCNConv, GATConv, GINConv, SAGEConv
 from torch_geometric.nn import global_mean_pool
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
 
-# Model 1
+################################ MACHINE LEARNING MODELS ################################
+#########################################################################################
+
+# ML Models With Hyperparameters
+ML_Models = {
+"LR": LinearRegression(fit_intercept=True, positive=False), # ML Model 1
+"ENet": ElasticNet(alpha=0.01, l1_ratio=0.2, max_iter=1000,random_state=42), # ML Model 2
+"SVM": SVR(C=10, epsilon=0.01, gamma="scale", kernel="rbf"), # ML Model 3
+"RF": RandomForestRegressor(n_estimators=100, max_depth=None, max_features="sqrt", # ML Model 4
+				min_samples_leaf=1, min_samples_split=2, random_state=42)
+}
+
+################################ DEEP LEARNING MODELS ###################################
+#########################################################################################
+
+# GNN Model 1
 class SimpleGCN(torch.nn.Module):
 	def __init__(self, num_features, hidden_dim, num_classes):
 		super().__init__()
@@ -19,7 +38,7 @@ class SimpleGCN(torch.nn.Module):
 		x = self.fc(x) # FC regression
 		return x
 
-# Model 2
+# GNN Model 2
 class SimpleGAT(torch.nn.Module):
 	def __init__(self, num_features, hidden_dim, num_classes):
 		super().__init__()
@@ -39,7 +58,7 @@ class SimpleGAT(torch.nn.Module):
 		x = self.fc(x) # FC regression
 		return x
 
-# Model 3
+# GNN Model 3
 class SimpleGIN(torch.nn.Module):
 	def __init__(self, num_features, hidden_dim, num_classes):
 		super().__init__()
@@ -58,7 +77,7 @@ class SimpleGIN(torch.nn.Module):
 		x = self.fc(x) # FC regression
 		return x
 
-# Model 4
+# GNN Model 4
 class SimpleGraphSAGE(nn.Module):
 	def __init__(self, num_features, hidden_dim, num_classes):
 		super().__init__()
@@ -73,3 +92,4 @@ class SimpleGraphSAGE(nn.Module):
 		x = global_mean_pool(x, batch) # Global pooling
 		x = self.fc(x) # FC regression
 		return x
+
