4845749fd9454048d9fbc4b0048a8744bad78d65
rajanbit <rajan22091998@gmail.com>
2026-01-28 09:21:29 +0530

    updated dataloader, model and train_test scripts for GNN_FP analysis

--- src/model.py
+++ src/model.py
@@ -93,3 +93,160 @@ class SimpleGraphSAGE(nn.Module):
 		x = self.fc(x) # FC regression
 		return x
 
+
+# GNN_FP Model 1
+class SimpleGCN_FP(torch.nn.Module):
+	def __init__(self, num_features, hidden_dim, num_classes):
+		super().__init__()
+
+		# GNN branch
+		self.conv = GCNConv(num_features, hidden_dim) # GCN layer
+		self.fc = torch.nn.Linear(hidden_dim, hidden_dim) # FC(fully connected/dense) layer
+
+		# FP branch
+		self.fc_fp = torch.nn.Linear(2048, hidden_dim) # FC for FP
+
+		# Regression layer
+		self.fc_final = torch.nn.Linear(hidden_dim * 2, num_classes) # Final FC for regression
+
+	def forward(self, x, edge_index, batch, fp):
+
+		# Graph Features
+		x = self.conv(x, edge_index) # GCN Conv
+		x = relu(x) # ReLU
+		x = global_mean_pool(x, batch) # Global pooling
+		x = self.fc(x) # FC regression
+		x = relu(x) # ReLU
+
+		# Fingerprint features
+		fp = self.fc_fp(fp)
+		fp = relu(fp)
+
+		# Concat both graph and FP features
+		combined = torch.cat([x, fp], dim=1)
+
+		# Regression
+		out = self.fc_final(combined)
+
+		# Return output
+		return out
+
+# GNN_FP Model 2
+class SimpleGAT_FP(torch.nn.Module):
+	def __init__(self, num_features, hidden_dim, num_classes):
+		super().__init__()
+
+		# GNN branch
+		self.conv = GATConv(num_features, hidden_dim, 
+					heads=8, # Single attention head
+					concat = False,
+					dropout=0.6, # Dropout from original paper
+					negative_slope=0.2) # LeakyReLU original paper
+		self.fc = torch.nn.Linear(hidden_dim, hidden_dim) # FC(fully connected/dense) layer
+
+		# FP branch
+		self.fc_fp = torch.nn.Linear(2048, hidden_dim) # FC for FP
+
+		# Regression layer
+		self.fc_final = torch.nn.Linear(hidden_dim * 2, num_classes) # Final FC for regression
+
+	def forward(self, x, edge_index, batch, fp):
+
+		# Graph Features
+		x = self.conv(x, edge_index) # GCN Conv
+		x = relu(x) # ReLU
+		x = global_mean_pool(x, batch) # Global pooling
+		x = self.fc(x) # FC regression
+		x = relu(x) # ReLU
+
+		# Fingerprint features
+		fp = self.fc_fp(fp)
+		fp = relu(fp)
+
+		# Concat both graph and FP features
+		combined = torch.cat([x, fp], dim=1)
+
+		# Regression
+		out = self.fc_final(combined)
+
+		# Return output
+		return out
+
+# GNN_FP Model 3
+class SimpleGIN_FP(torch.nn.Module):
+	def __init__(self, num_features, hidden_dim, num_classes):
+		super().__init__()
+
+		# GNN branch
+	        # MLP for GIN
+		self.mlp = nn.Linear(num_features, hidden_dim)
+		# GIN layer
+		self.conv = GINConv(nn=self.mlp, train_eps=False)
+		self.fc = torch.nn.Linear(hidden_dim, hidden_dim) # FC(fully connected/dense) layer
+
+		# FP branch
+		self.fc_fp = torch.nn.Linear(2048, hidden_dim) # FC for FP
+
+		# Regression layer
+		self.fc_final = torch.nn.Linear(hidden_dim * 2, num_classes) # Final FC for regression
+
+	def forward(self, x, edge_index, batch, fp):
+
+		# Graph Features
+		x = self.conv(x, edge_index) # GCN Conv
+		x = relu(x) # ReLU
+		x = global_mean_pool(x, batch) # Global pooling
+		x = self.fc(x) # FC regression
+		x = relu(x) # ReLU
+
+		# Fingerprint features
+		fp = self.fc_fp(fp)
+		fp = relu(fp)
+
+		# Concat both graph and FP features
+		combined = torch.cat([x, fp], dim=1)
+
+		# Regression
+		out = self.fc_final(combined)
+
+		# Return output
+		return out
+
+
+# GNN Model 4
+class SimpleGraphSAGE_FP(nn.Module):
+	def __init__(self, num_features, hidden_dim, num_classes):
+		super().__init__()
+
+		# GNN branch
+		# GraphSAGE Layer
+		self.conv = SAGEConv(num_features, hidden_dim, aggr="mean")
+		self.fc = torch.nn.Linear(hidden_dim, hidden_dim) # FC(fully connected/dense) layer
+
+		# FP branch
+		self.fc_fp = torch.nn.Linear(2048, hidden_dim) # FC for FP
+
+		# Regression layer
+		self.fc_final = torch.nn.Linear(hidden_dim * 2, num_classes) # Final FC for regression
+
+	def forward(self, x, edge_index, batch, fp):
+
+		# GNN Features
+		x = self.conv(x, edge_index) # GCN Conv
+		x = relu(x) # ReLU
+		x = global_mean_pool(x, batch) # Global pooling
+		x = self.fc(x) # FC regression
+		x = relu(x) # ReLU
+
+		# Fingerprint features
+		fp = self.fc_fp(fp)
+		fp = relu(fp)
+
+		# Concat both graph and FP features
+		combined = torch.cat([x, fp], dim=1)
+
+		# Regression
+		out = self.fc_final(combined)
+
+		# Return output
+		return out
--- src/train_test.py
+++ src/train_test.py
@@ -8,7 +8,7 @@ warnings.filterwarnings(
     message="Attempting to run cuBLAS"
 )
 
-# Model training function
+# Model training function | GNN models
 def TrainGNN(model, training_loader, epochs=10, learning_rate=0.01):
 
 	'''
@@ -54,7 +54,7 @@ def TrainGNN(model, training_loader, epochs=10, learning_rate=0.01):
 	# Return trained model
 	return model
 
-# Model testing function
+# Model testing function | GNN models
 def TestGNN(model, testing_loader):
 
 	'''
@@ -99,3 +99,93 @@ def TestGNN(model, testing_loader):
 	# Return y_test and y_pred
 	return y_test, y_pred
 
+# Model training function | GNN + FP models
+def TrainGNN_FP(model, training_loader, epochs=10, learning_rate=0.01):
+
+	'''
+	This function take model, dataloader, number of epochs, learning rate 
+	as input and train the model till n epochs using dataset from dataloader
+
+	Args:
+		model:  GNN model
+			Pytorch model object
+
+		training_loader: Training data loader object
+				Geometric DataLoader object
+
+		epochs: Number of epochs
+			Int object
+
+		learning_rate: Learning rate
+			Float object
+
+	Return:
+		Trained model
+	'''
+
+	# Optimizer
+	optimizer = Adam(model.parameters(), lr=learning_rate)
+
+	# Device
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	
+	# Load model to device
+	model = model.to(device)
+
+	# Training loop
+	for e in range(epochs):
+		for data in training_loader:
+			data = data.to(device)
+			optimizer.zero_grad()
+			out = model(data.x, data.edge_index, data.batch, data.fp)
+			loss = mse_loss(out.view(-1).cpu(), data.y.view(-1).cpu())
+			loss.backward()
+			optimizer.step()
+
+	# Return trained model
+	return model
+
+# Model testing function | GNN + FP models
+def TestGNN_FP(model, testing_loader):
+
+	'''
+	This function take trained model and test dataloader, to perform
+	prediction for evaluating the model's performance on test set
+
+	Args:
+		model:  GNN model
+			Pytorch model object
+
+		testing_loader: Validation/Testing data loader object
+				Geometric DataLoader object
+
+	Return:
+		list for y_test and y_pred
+	'''
+
+	model.eval()
+	y_pred, y_test = [], []
+
+	# Device
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	
+	# Load model to device
+	model = model.to(device)
+
+	with torch.no_grad():
+
+		# Validation/Testing loop
+		for data in testing_loader:
+			data = data.to(device)
+			out = model(data.x, data.edge_index, data.batch, data.fp)
+			out = out.view(-1).cpu()
+			y = data.y.view(-1).cpu()
+			y_pred.append(out)
+			y_test.append(y)
+
+	# Flattening tensor
+	y_test = torch.cat(y_test).tolist()
+	y_pred = torch.cat(y_pred).tolist()
+
+	# Return y_test and y_pred
+	return y_test, y_pred
